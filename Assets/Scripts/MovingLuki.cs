2bb22aeb2907223124405dece2a7dfb65aebabbc
wiverite <wiverite@gmail.com>
2022-03-26 15:44:13 +0100

    luggasch can walk now :0

--- Assets/Scripts/MovingLuki.cs
+++ Assets/Scripts/MovingLuki.cs
@@ -11,7 +11,7 @@ public class MovingLuki : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        lukiii = GameObject.FindGameObjectWithTag("PLayer");
+        lukiii = GameObject.FindGameObjectWithTag("Player");
     }
 
     // Update is called once per frame
